f87cc603ad43a9043895579c7973444e73d05406
Vivekkv10 <vivekkv994@gmail.com>
2022-12-20 13:07:21 +0530

    change after branch rule and created new feature branch called featureBranch

--- fieldMaping.js
+++ fieldMaping.js
@@ -1,3 +1,4 @@
 // First Line
 // this is second line created by vivekkv10 from local repo vscode
 // third line after creating branch protection rule..and this should not be commiting
+// fourth line after creating branch, all lines will be commited to featureBranch 
